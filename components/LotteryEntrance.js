66a46bfafd7c03d02986707aab766e44b0d983bc
Jaffar <jacepac264@gmail.com>
2022-07-12 10:41:41 +0300

    handled chain changes

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -9,7 +9,7 @@ export default function LotteryEntrance() {
     const [entranceFee, setEntranceFee] = useState(0)
     const [numPlayers, setNumPlayers] = useState(0)
     const [recentWinner, setRecentWinner] = useState(0)
-    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const { chainId: chainIdHex, isWeb3Enabled, account } = useMoralis()
     const chainId = parseInt(chainIdHex)
     const lotteryAddress = chainId in contractAddress ? contractAddress[chainId][0] : null
     const dispatch = useNotification()
@@ -78,30 +78,42 @@ export default function LotteryEntrance() {
 
     return (
         <div className="p-5 text-xl">
-            {lotteryAddress ? (
-                <div>
-                    <div className="m-3"><strong>Entrance Fee:</strong> {ethers.utils.formatEther(entranceFee)} <strong className="text-red-900">AVAX</strong></div>
-                    <div className="m-3"><strong>Current Number of players:</strong> {numPlayers}</div>
-                    <div className="m-3"><strong>Most Recent Winner:</strong> <span className="italic">{recentWinner}</span></div>
-                    <button
-                        className="bg-stone-700 hover:bg-stone-800 text-slate-200 rounded px-3 py-2 disabled:opacity-50 disabled:hover:bg-stone-700 m-3"
-                        onClick={async () => {
-                            await enterLottery({
-                                onSuccess: handleSuccess,
-                                onError: (error) => console.log(error),
-                            })
-                        }}
-                        disabled={isLoading || isFetching}
-                    >
-                        {isLoading || isFetching ? (
-                            <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
-                        ) : (
-                            <div>Enter Lottery</div>
-                        )}
-                    </button>
-                </div>
+            {account ? (
+                (chainId == 43113) ? (
+                    <div>
+                        <div className="m-3">
+                            <strong>Entrance Fee:</strong> {ethers.utils.formatEther(entranceFee)}{" "}
+                            <strong className="text-red-900">AVAX</strong>
+                        </div>
+                        <div className="m-3">
+                            <strong>Current Number of players:</strong> {numPlayers}
+                        </div>
+                        <div className="m-3">
+                            <strong>Most Recent Winner:</strong>{" "}
+                            <span className="italic">{recentWinner}</span>
+                        </div>
+                        <button
+                            className="bg-stone-700 hover:bg-stone-800 text-slate-200 rounded px-3 py-2 disabled:opacity-50 disabled:hover:bg-stone-700 m-3"
+                            onClick={async () => {
+                                await enterLottery({
+                                    onSuccess: handleSuccess,
+                                    onError: (error) => console.log(error),
+                                })
+                            }}
+                            disabled={isLoading || isFetching}
+                        >
+                            {isLoading || isFetching ? (
+                                <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+                            ) : (
+                                <div>Enter Lottery</div>
+                            )}
+                        </button>
+                    </div>
+                ) : (
+                    <div>Please switch to Fuji testnet</div>
+                )
             ) : (
-                <div>Please switch to Fuji testnet</div>
+                <div>Please connect to a wallet</div>
             )}
         </div>
     )
